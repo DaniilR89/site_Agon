9d19bdbe274647c21c143a28b2aa836ddd60b454
Daniil <93051999+DaniilR89@users.noreply.github.com>
2023-06-20 00:04:04 +0300

    Add files via upload

---
+++ Agon/js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
+
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+
+btn.addEventListener('click', ()=>{
+    nav.classList.toggle('menu-open');
+});
